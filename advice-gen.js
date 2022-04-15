dec97119b705d699bf31c6d9460fa291c9860752
juliosam <ja.samaniegom@gmail.com>
2022-04-15 00:33:41 -0500

    ready for review

--- advice-gen.js
+++ advice-gen.js
@@ -1,4 +1,16 @@
-const hello = "hello"
+const boton = document.querySelector(".card__button");
+const consejo = document.querySelector(".card__paragraph-inside")
+const adviceID = document.querySelector(".card__title-number")
 
-console.log(hello)
+const azar = function(){ return  Math.floor(Math.random()*(224-0))}
 
+const fetcher = function(){
+fetch(`https://api.adviceslip.com/advice/${azar()}`)
+.then(res=>res.json())
+.then(data => {
+    consejo.innerHTML = data.slip.advice
+    adviceID.innerHTML = data.slip.id
+     })
+}
+
+boton.addEventListener('click', fetcher)
